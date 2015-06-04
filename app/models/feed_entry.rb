2f48c7909e8f1069e10c5d43dd7532ba801f5bca
Wheezy123 <johnsonjeromec@gmail.com>
2015-06-03 23:06:52 -0500

    updated feedEntry model" " "

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -1,18 +1,33 @@
 class FeedEntry < ActiveRecord::Base
+  attr_accessible :guid, :url, :title, :summary, :published_at
 
-  def self.update_from_feed(feed_url)
-    feed = Feedjira::Feed.fetch_and_parse(feed_url)
+  def self.update_from_feed(feed_name)
+    feed = Feed.find_by_name(feed_name)
+    feed_data = Feedjira::Feed.fetch_and_parse(feed.feed_url)
 
-    feed.entries do |entry|
-      unless exists? :guid => entry.id
-        create!(
-          name: entry.title,
-          summary: entry.summary,
-          url: entry.url,
-          published_at: entry.published,
-          guid: entry.id
-        )
+    add_entries(feed_data.entries, feed)
+  end
+
+
+
+
+
+
+
+
+    private
+      def self.add_entries(entries, feed)
+        entries.each do |entry|
+          break if exists? :entry_id => entry.id
+          create!(
+            entry_id: entry.id,
+            url: entry.url,
+            title: entry.title.sanitize,
+            summary: entry.content.sanitize,
+            published_at: entry.published
+          )
+        end
       end
     end
-  end
+
 end
