ed9cc596a398cd17ab20b7c6e87d824d29da51e4
Wheezy123 <johnsonjeromec@gmail.com>
2015-06-03 22:09:48 -0500

    add function to reed feeds

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -1,2 +1,17 @@
 class FeedEntry < ActiveRecord::Base
+
+  def self.update_from_feed(feed_url)
+    feed = Feedjira::Feed.fetch_and_parse(feed_url)
+
+    feed.entries do |entry|
+      unless exists? :guid => entry.id
+        create!(
+          name: entry.title,
+          summary: entry.summary,
+          url: entry.url,
+          published_at: entry.published,
+          guid: entry.id
+        )
+    end
+  end
 end
