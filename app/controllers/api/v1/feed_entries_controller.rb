1d76b978040a1e6304f66a060671b08ff57a375d
Wheezy123 <johnsonjeromec@gmail.com>
2015-07-08 21:54:48 -0500

    added method to grab cnn feeds and save to db

--- app/controllers/api/v1/feed_entries_controller.rb
+++ app/controllers/api/v1/feed_entries_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::FeedEntriesController < ApplicationController
+  respond_to :json
+
+  def show
+    @stories = FeedEntry.all
+
+    render json: @stories
+  end
 end
--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -1,11 +1,24 @@
 class FeedEntry < ActiveRecord::Base
-  attr_accessor :guid, :url, :title, :summary, :published_at
+  #attr_accessor :guid, :url, :title, :summary, :published_at
 
   def self.update_from_feed(feed_name)
     feed = Feed.find_by_name(feed_name)
     feed_data = Feedjira::Feed.fetch_and_parse(feed.feed_url)
 
     add_entries(feed_data.entries, feed)
+  end
+
+
+  def self.get_cnn_stories
+    url = 'http://rss.cnn.com/rss/cnn_topstories.rss'
+    obj = Feedjira::Feed.fetch_and_parse(url)
+    obj.each do |entry|
+      FeedEntry.create(
+        title: entry.title,
+        url: entry.url,
+        summary: entry.summary
+      )
+    end
   end
 
 
